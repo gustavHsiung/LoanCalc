f471b40de4052469ec5850acd39e5decc60527c5
gustavHsiung <knightgustav@gmail.com>
2012-02-15 22:27:34 +0800

    add input element
    1. add textfield
    2. add toolbar button 'Done' to keyboard for iPhone.

--- Resources/app.js
+++ Resources/app.js
@@ -1,7 +1,12 @@
+//application variables
+var numberMonths = 36; //loan length
+var interestRate = 6.0; //interest rate
+
+
+
 // this sets the background color of the master UIView (when there are no windows/tab groups on it)
 Titanium.UI.setBackgroundColor('#000');
-
-
+Titanium.UI.setBackgroundImage('background.jpg');
 //
 // create base UI tab and root window
 //
@@ -14,11 +19,11 @@ var pHeight = Ti.Platform.displayCaps.platformHeight;
 
 //create the window
 var win1 = Titanium.UI.createWindow({  
+    title: 'LaonCalc',
     width:pWidth,
     height:pHeight,
     top:0,
     left:0,
-    backgroundImage:'background.jpg'
 });
 
 //create the view
@@ -35,37 +40,118 @@ var view = Titanium.UI.createView({
 //logo left margin
 var _logoWidth  = 255
 var _logoHeight = 255
-var _logoMarginLeft = (view.width - _logoWidth)/2
-var _logoMarginTop = (view.height - _logoHeight)/2
+var _logoMarginLeft = view.width - _logoWidth/2
+var _logoMarginTop  = (view.height - _logoHeight)/2
 
 var logo = Titanium.UI.createImageView({
 		image: 'logo.png',
 		width:  _logoWidth,
 		height: _logoHeight,
 		left:   _logoMarginLeft,
-		top: 	_logoMarginTop
+		top: 	-_logoMarginTop,
 });
 
 view.add(logo);
 
-var label1 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'Learson One',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
-view.add(label1);
-
-var label2 = Titanium.UI.createLabel({
+var amountLabel = Titanium.UI.createLabel({
 	color:'#232',
-	text:'Titanium Cook Book',
-	font:{fontSize:16,fontFamily:'Helvetica Neue'},
+	text:'Loan amount:   $',
+	font:{fontSize:14,fontFamily:'Helvetica Neue', fontWeight:'bold'},
 	textAlign:'center',
-	top:_logoMarginTop+_logoHeight +10,
-	width:'auto'
+	width:'auto',
+	height:30,
+	top:100,
+	left:20
 });
-view.add(label2);
+view.add(amountLabel);
+
+var interestRateLabel = Titanium.UI.createLabel({
+	color:'#232',
+	text:'Interest Rate:  %',
+	font:{fontSize:14,fontFamily:'Helvetica Neue', fontWeight:'bold'},
+	textAlign:'left',
+	width:'auto',
+	height:30,
+	top:150,
+	left:20
+});
+view.add(interestRateLabel);
+
+var loanLengthLabel = Titanium.UI.createLabel({
+	color:'#232',
+	text:'Loan length (' + numberMonths + ' months):',
+	font:{fontSize:14,fontFamily:'Helvetica Neue', fontWeight:'bold'},
+	textAlign:'left',
+	width:'auto',
+	height:30,
+	top:200,
+	left:20
+});
+view.add(loanLengthLabel);
+
+//flexible space
+var flexSpace = Titanium.UI.createButton({
+	systemButton:Titanium.UI.iPhone.SystemButton.FLEXIBLE_SPACE
+})
+
+//done system button
+var doneButton = Titanium.UI.createButton({
+	systemButton:Titanium.UI.iPhone.SystemButton.DONE,
+	bottom:0
+})
+
+var amountTextField = Titanium.UI.createTextField({
+	hintText: '1000.00',
+	width:140,
+	height:30,
+	top:100,
+	right:20,
+	keyboardToolbar: [flexSpace,doneButton],
+	borderStyle:Titanium.UI.INPUT_BORDERSTYLE_ROUNDED,
+	returnKeyType:Titanium.UI.RETURNKEY_DONE,
+	keyboardType:Titanium.UI.KEYBOARD_NUMBER_PAD
+})
+view.add(amountTextField);
+
+var interestRateTextField = Titanium.UI.createTextField({
+	value: interestRate,
+	width:140,
+	height:40,
+	top:interestRateLabel.top,
+	right:20,
+	keyboardToolbar: [flexSpace,doneButton],
+	borderStyle:Titanium.UI.INPUT_BORDERSTYLE_ROUNDED,
+	returnKeyType:Titanium.UI.RETURNKEY_DONE
+})
+
+interestRateTextField.addEventListener('focus',function(e){
+	if(Ti.Platform.osname == 'iPhone'){
+		interestRateTextField.top = 100;
+		interestRateLable.top = 100;
+		amountTextField.visible = false;
+		amountLabel.visible = false;
+	}
+})
+view.add(interestRateTextField);
+
+//click event listener
+doneButton.addEventListener('click',function(e){
+	Ti.API.info("Available memory: " + Ti.Platform.availableMemory);
+    
+	amountTextField.blur();
+	interestRateTextField.blur();
+	
+/*
+	interestRateTextField.top = 150;
+	interestRateLabel.top = 150;
+	interestRate = interestRateTextField.value;
+	amountTextField.visible = true;
+	amountLabel.visible = true;*/
+	Ti.API.info("Available memory: " + Ti.Platform.availableMemory);
+   
+})
+
+
 
 win1.add(view);
 
